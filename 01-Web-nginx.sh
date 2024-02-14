a478f0c130857e7d6362b5c9e10dbae4017fddef
vijaybiloo <dpk.vijay@gmail.com>
2024-02-14 18:46:32 +0530

    updated vijay as centos

--- 01-Web-nginx.sh
+++ 01-Web-nginx.sh
@@ -53,7 +53,7 @@ VALIDATE $? "changing the directory to html"
 unzip /opt/web.zip &>>$LOGFILE
 VALIDATE $? "unziping the roboshop zip file"
 
-cp /home/vijay/roboshop-documentation/roboshop.conf  /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
+cp /home/centos/roboshop-documentation/roboshop.conf  /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
 VALIDATE $? "Coping the roboshop.conf"
 
 systemctl restart nginx &>>$LOGFILE
