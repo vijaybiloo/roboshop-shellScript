1ece75498e1e1d7b6e44342c8c8812d28a7d4196
vijaybiloo <dpk.vijay@gmail.com>
2024-02-15 19:03:34 +0530

    updated the catalogue file

--- 02-catalogue.sh
+++ 02-catalogue.sh
@@ -89,5 +89,5 @@ VALIDATE $? "Copying the file mongo.repo"
 yum install mongodb-org-shell -y &>>$LOGFILE
 VALIDATE $? "Installing mongo-org-shell"
 
-mongo --host mongodb.jiondevops.site </tmp/app/schema/catalogue.js &>>$LOGFILE
+mongo --host mongodb.jiondevops.site </app/schema/catalogue.js &>>$LOGFILE
 VALIDATE $? "Loading schema"
